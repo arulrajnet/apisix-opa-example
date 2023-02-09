8d0ea012f512418bcede3c9e9712b5f21bf8defd
Arul <me@arulraj.net>
2023-02-09 07:08:34 +0530

    typo fix

--- apisix_conf/apisix-config-init.sh
+++ apisix_conf/apisix-config-init.sh
@@ -10,17 +10,17 @@ CURL_OPTS="--silent --show-error --fail"
 # Delete
 
 ## routes
-routes=$(curl $CURL_OPTS --fail -X GET -H "X-API-KEY: ${APISIX_SERVICE_ADMIN_SECRET}" http://${APISIX_SERVICE_HOST}:${APISIX_SERVICE_ADMIN_PORT}/apisix/admin/routes | jq -r '.list[]|.value.id')
+routes=$(curl $CURL_OPTS -X GET -H "X-API-KEY: ${APISIX_SERVICE_ADMIN_SECRET}" http://${APISIX_SERVICE_HOST}:${APISIX_SERVICE_ADMIN_PORT}/apisix/admin/routes | jq -r '.list[]|.value.id')
 
 for route in $routes; do
-  curl $CURL_OPTS --fail -X DELETE -H "X-API-KEY: ${APISIX_SERVICE_ADMIN_SECRET}" http://${APISIX_SERVICE_HOST}:${APISIX_SERVICE_ADMIN_PORT}/apisix/admin/routes/$route
+  curl $CURL_OPTS -X DELETE -H "X-API-KEY: ${APISIX_SERVICE_ADMIN_SECRET}" http://${APISIX_SERVICE_HOST}:${APISIX_SERVICE_ADMIN_PORT}/apisix/admin/routes/$route
 done
 
 ## services
-services=$(curl $CURL_OPTS --fail -X GET -H "X-API-KEY: ${APISIX_SERVICE_ADMIN_SECRET}" http://${APISIX_SERVICE_HOST}:${APISIX_SERVICE_ADMIN_PORT}/apisix/admin/services | jq -r '.list[]|.value.id')
+services=$(curl $CURL_OPTS -X GET -H "X-API-KEY: ${APISIX_SERVICE_ADMIN_SECRET}" http://${APISIX_SERVICE_HOST}:${APISIX_SERVICE_ADMIN_PORT}/apisix/admin/services | jq -r '.list[]|.value.id')
 
 for service in $services; do
-  curl $CURL_OPTS --fail -X DELETE -H "X-API-KEY: ${APISIX_SERVICE_ADMIN_SECRET}" http://${APISIX_SERVICE_HOST}:${APISIX_SERVICE_ADMIN_PORT}/apisix/admin/services/$service
+  curl $CURL_OPTS -X DELETE -H "X-API-KEY: ${APISIX_SERVICE_ADMIN_SECRET}" http://${APISIX_SERVICE_HOST}:${APISIX_SERVICE_ADMIN_PORT}/apisix/admin/services/$service
 done
 
 # Create
@@ -28,7 +28,7 @@ done
 ## service
 
 ### Web1
-curl $CURL_OPTS --fail -X PUT -H "X-API-KEY: ${APISIX_SERVICE_ADMIN_SECRET}" http://${APISIX_SERVICE_HOST}:${APISIX_SERVICE_ADMIN_PORT}/apisix/admin/services/1 -d '
+curl $CURL_OPTS -X PUT -H "X-API-KEY: ${APISIX_SERVICE_ADMIN_SECRET}" http://${APISIX_SERVICE_HOST}:${APISIX_SERVICE_ADMIN_PORT}/apisix/admin/services/1 -d '
 {
     "upstream": {
         "type": "roundrobin",
@@ -42,7 +42,7 @@ curl $CURL_OPTS --fail -X PUT -H "X-API-KEY: ${APISIX_SERVICE_ADMIN_SECRET}" htt
 ## routes
 
 ### Web1
-curl $CURL_OPTS --fail -X PUT -H "X-API-KEY: ${APISIX_SERVICE_ADMIN_SECRET}" http://${APISIX_SERVICE_HOST}:${APISIX_SERVICE_ADMIN_PORT}/apisix/admin/routes/11 -d '
+curl $CURL_OPTS -X PUT -H "X-API-KEY: ${APISIX_SERVICE_ADMIN_SECRET}" http://${APISIX_SERVICE_HOST}:${APISIX_SERVICE_ADMIN_PORT}/apisix/admin/routes/11 -d '
 {
     "uri": "/*",
     "service_id": "1",
